1180dfa4cfdf8d9efdc00b4a48b96fdbefc34d0b
Bérénice Faltrept <berenice.faltrept@gmail.com>
2015-09-24 16:12:52 +0200

    ajout IdType

---
+++ IdType.java
@@ -0,0 +1,10 @@
+public class IdType {
+	
+  /* terminals */
+  public static final int INTEGER = 1;
+  public static final int CHARACTER = 2;
+  public static final int FLOAT = 3;
+  public static final int BOOLEAN = 4;
+  public static final int STRING = 5;
+
+}
